5f2eed17940e7bed372e2126ecb105bcf37d26c6
Denis Saplitsyn <packman17@mail.ru>
2025-09-24 00:44:33 +0300

    add start command in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.handler.HibernateHandler;
+import org.example.service.UserService;
+import org.example.service.UserServiceHibernateImpl;
+import org.example.ui.ConsoleReader;
+import org.example.ui.ConsoleUI;
+
 public class Main {
     public static void main(String[] args) {
+        UserService userService = new UserServiceHibernateImpl();
+        ConsoleReader consoleReader = new ConsoleReader();
 
+        ConsoleUI consoleUI = new ConsoleUI(userService, consoleReader);
+
+        consoleUI.start();
+
+        HibernateHandler.closeSessionFactory();
     }
 }
