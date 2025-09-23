03429818b147ef7b5a3998263d9155d1cd868951
Denis Saplitsyn <packman17@mail.ru>
2025-09-23 20:22:17 +0300

    add entity User

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,87 @@
+package org.example.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email",
+            unique = true,
+            nullable = false)
+    private String email;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public User(LocalDateTime createdAt) {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User(long id, String name, String email, Integer age, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
